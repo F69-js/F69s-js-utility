b5b004b193c2ee3c846974b6f2c6e8a40f7e74db
F69minrcraft <ikoi22323@outlook.jp>
2026-03-28 19:05:50 +0900

    Add Alias in Export expr

--- bandle.js
+++ bandle.js
@@ -1,4 +1,4 @@
 import {CustomError} from "./parts/CustomError"
 import {CustomEventModule} from "./parts/CustomEvent"
 import {Fjalu,Fjala,LanguageModule} from "./parts/fjalanfjalu"
-export {CustomError,CustomEventModule,Fjalu,Fjala,LanguageModule}
+export {CustomError,CustomEventModule,Fjalu as FjaluEngine,Fjala as FjalaTranslator,LanguageModule as LangLib}
