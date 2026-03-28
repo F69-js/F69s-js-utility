a006419359d4234e566be4567907d7652ff827fa
F69minrcraft <ikoi22323@outlook.jp>
2026-03-28 18:59:01 +0900

    Create Bundle.js

---
+++ bandle.js
@@ -0,0 +1,4 @@
+import {CustomError} from "./parts/CustomError"
+import {CustomEventModule} from "./parts/CustomEvent"
+import {Fjalu,Fjala,LanguageModule} from "./parts/fjalanfjalu"
+export {CustomError,CustomEventModule,Fjalu,Fjala,LanguageModule}
